c6e0e0a8a8599bf77b77c2ea2f47c1a8956e878e
Jing Si <jingsi@Jings-MacBook-Pro.local>
2017-04-24 17:59:58 -0400

    Initial Commit

---
+++ Lab3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab3
+//
+//  Created by Jing Si on 4/24/17.
+//  Copyright © 2017 Jing Si. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
